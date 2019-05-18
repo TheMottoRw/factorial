d44deeac5211d99ab1ecd1a8e734e822f64597c2
TheMottoRW <hasua.mr@gmail.com>
2019-05-18 12:31:17 +0200

    [chore] fix factorial addition to multiplication

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-import http from 'http';
-export const factorial = (number) => {
-    let fact = 1;
-    if (number !==0) {
-        fact += number;
+const http = require('http');
+let fact = 1;
+const factorial = (number) => {
+    if (number !== 0) {
+        fact *= number;
         factorial(--number);
     }
     return fact;
 } 
-http.createServer(3000, () =>{
-    
-})
+http.createServer( (req, res) =>{
+    console.log(factorial(5));
+}).listen(1000);
+console.log('Server started listening to port 1000');
